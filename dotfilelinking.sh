5ed490ff862e49e7b7ead75a785488c3dad5daba
NULLCT <cockatiel.u10@gmail.com>
2020-03-01 14:26:43 +0900

    fix path names

--- dotfilelinking.sh
+++ dotfilelinking.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-ln -sf ~/dotfile/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfile/dein.toml ~/.config/nvim/dein.toml
-ln -sf ~/dotfile/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
-ln -sf ~/dotfile/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/dotfile/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfile/.zshenv ~/.zshenv
-ln -sf ~/dotfile/.zshrc ~/.config/zsh/.zshrc
+ln -sf ~/Dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/Dotfiles/dein.toml ~/.config/nvim/dein.toml
+ln -sf ~/Dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
+ln -sf ~/Dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/Dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/Dotfiles/.zshenv ~/.zshenv
+ln -sf ~/Dotfiles/.zshrc ~/.config/zsh/.zshrc
 
 
