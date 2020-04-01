e9ea7023e2e4e753c45e3e26ef77f33ff06c1fba
nari <nari@Nari-MBP.local>
2020-04-01 18:59:21 +0900

    'master'

--- script/dotfilelinking.sh
+++ script/dotfilelinking.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-ln -sf ~/Dotfiles/init.vim ~/.config/nvim/init.vim
-ln -sf ~/Dotfiles/dein.toml ~/.config/nvim/dein.toml
-ln -sf ~/Dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
 ln -sf ~/Dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/Dotfiles/.zshenv ~/.zshenv
 ln -sf ~/Dotfiles/.zshrc ~/.config/zsh/.zshrc
 ln -sf ~/Dotfiles/ignore ~/.config/git/ignore
+ln -sf ~/Dotfiles/.vimrc ~/.vimrc
