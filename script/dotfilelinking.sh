82059f368dd687a74dc200938c6047491323908d
nari <nari@Nari-MBP.local>
2020-05-09 03:32:54 +0900

    I'm happy cause tmux attachedxdg-config

--- script/dotfilelinking.sh
+++ script/dotfilelinking.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/Dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/Dotfiles/ignore ~/.config/git/ignore
-ln -sf ~/Dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/Dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 ln -sf ~/Dotfiles/.zshenv ~/.zshenv
 ln -sf ~/Dotfiles/.zshrc ~/.zshrc
 ln -sf ~/Dotfiles/.vimrc ~/.vimrc
