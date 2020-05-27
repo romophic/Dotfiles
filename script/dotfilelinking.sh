bbf536e827f255c46c187ceda0406cc174339fe3
NULLCT <cockatiel.u10@gmail.com>
2020-05-27 14:57:58 +0900

    fix tmux conf file path and name and hogehoge

--- script/dotfilelinking.sh
+++ script/dotfilelinking.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/Dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/Dotfiles/ignore ~/.config/git/ignore
-ln -sf ~/Dotfiles/tmux.conf ~/.config/tmux/tmux.conf
+ln -sf ~/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/Dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/Dotfiles/dein.toml ~/.config/nvim/dein.toml
 ln -sf ~/Dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
