dea29976a5fbc47eab3a2b9110211c19ec3295ca
NULLCT <cockatiel.u10@gmail.com>
2020-06-19 23:04:42 +0900

    change name to use i3wm and rofi

--- script/dotfilelinking.sh
+++ script/dotfilelinking.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
+
+#alacritty
 ln -sf ~/Dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/Dotfiles/ignore ~/.config/git/ignore
-ln -sf ~/Dotfiles/config ~/.config/i3/config
-ln -sf ~/Dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/Dotfiles/init.vim ~/.config/nvim/init.vim
-ln -sf ~/Dotfiles/dein.toml ~/.config/nvim/dein.toml
-ln -sf ~/Dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
-ln -sf ~/Dotfiles/.zshenv ~/.zshenv
-ln -sf ~/Dotfiles/.zshrc ~/.zshrc
-ln -sf ~/Dotfiles/.vimrc ~/.vimrc
+
+#git
+ln -sf ~/Dotfiles/git_ignore ~/.config/git/ignore
+
+#i3
+ln -sf ~/Dotfiles/i3_config ~/.config/i3/config
+
+#vim
+ln -sf ~/Dotfiles/vimrc ~/.vimrc
+
+#neovim
+ln -sf ~/Dotfiles/neovim_init.vim ~/.config/nvim/init.vim
+ln -sf ~/Dotfiles/neovim_dein.toml ~/.config/nvim/dein.toml
+ln -sf ~/Dotfiles/neovim_dein_lazy.toml ~/.config/nvim/dein_lazy.toml
+
+#tmux
+ln -sf ~/Dotfiles/tmux.conf ~/.tmux.conf
+
+#zsh
+ln -sf ~/Dotfiles/zshrc ~/.zshrc
+ln -sf ~/Dotfiles/zshenv ~/.zshenv
