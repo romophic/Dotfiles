745d039d5ed623aebd5386ca89c3dff193d3cfec
NULLCT <cockatiel.u10@gmail.com>
2020-09-26 00:07:12 +0900

    change dotfileslinking again

--- script/dotfilelinking.sh
+++ script/dotfilelinking.sh
@@ -1,7 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
-#in ~/.config/
-ln -sf ~/Dotfiles/{alacritty,kitty,git,i3,i3blocks,nvim,ranger,rofi} ~/.config/
+#alacritty
+ln -sf ~/Dotfiles/alacritty/* ~/.config/alacritty/
 
-#in ~/
-ln -sf ~/Dotfiles/{vim,zsh,tmux}/.* ~/
+#git
+ln -sf ~/Dotfiles/git/* ~/.config/git/
+
+#i3
+ln -sf ~/Dotfiles/i3/config ~/.config/i3/config
+
+#i3blocks
+ln -sf ~/Dotfiles/i3blocks/config ~/.config/i3blocks/config
+
+#kitty
+ln -sf ~/Dotfiles/kitty/* ~/.config/kitty/
+
+#nvim
+ln -sf ~/Dotfiles/nvim/* ~/.config/nvim/
+
+#ranger
+ln -sf ~/Dotfiles/ranger/* ~/.config/ranger/
+
+#rofi
+ln -sf ~/Dotfiles/rofi/* ~/.config/rofi/
+
+#tmux
+ln -sf ~/Dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+#vim
+ln -sf ~/Dotfiles/vim/.vimrc ~/.vimrc
+
+#zsh
+ln -sf ~/Dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/Dotfiles/zsh/.zshenv ~/.zshenv
