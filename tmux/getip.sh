ca30a60bb3983c06b824cb1a98a4c294f21b2786
NULLCT <cockatiel.u10@gmail.com>
2021-08-25 15:15:40 +0900

    change ip shell

--- tmux/getip.sh
+++ tmux/getip.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
-hostname -I | awk '{print substr($0, 1, length($0)-1)}'
+
+function get_ip_wlan() {
+  no_of_broadcasts="$(ifconfig | grep broadcast | wc -l)"
+  if [ $no_of_broadcasts -gt "0" ]; then
+    ip="$(ifconfig | grep broadcast | awk '{print $2}' | tail -n1)"
+  fi
+}
+
+function get_ip_eth() {
+  ip="$(ifconfig | grep ether | awk '{print $2}')"
+  if [[ $ip != *"."* ]]; then
+    ip=""
+  fi
+}
+
+function print_ip() {
+  get_ip_wlan
+  if [ ! -z "$ip" ]; then
+    echo "$ip"
+  else
+    get_ip_eth
+    if [ ! -z "$ip" ]; then
+      echo $ip
+    else
+      echo ""
+    fi
+  fi
+}
+
+print_ip
