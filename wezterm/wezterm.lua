24faadfbb0b3275c0cffda644bd51f8cd981228c
NULLCT <cockatiel.u10@gmail.com>
2022-05-08 00:04:51 +0900

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require "wezterm";
+
+return {
+  color_scheme = "iceberg-dark",
+  font = wezterm.font("PlemolJP Console NF"),
+  font_size = 18,
+
+  window_background_opacity = 0.9,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+}
