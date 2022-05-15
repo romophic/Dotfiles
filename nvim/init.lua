ae5e0baa7ccad6643db0c14406bc2a6c046c2e1b
NULLCT <cockatiel.u10@gmail.com>
2022-05-15 15:57:57 +0900

    fix packer auto install script

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,10 @@
 --░▀▀▀░▀░▀░▀▀▀░░▀░░▀░░▀▀▀░▀▀▀░▀░▀░--
 ------------------------------------
 -- packer.nvim auto installer
-if vim.fn.empty(vim.fn.glob(vim.fn.stdpath("data").."/site/pack/packer/start/packer.nvim")) > 0 then
-  packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
@@ -59,11 +61,10 @@ require("cmp").setup {
   },
 }
 require("nvim-treesitter.configs").setup {
-  ensure_installed = "maintained",
-  ignore_install = { "norg" },
+  ensure_installed = "all",
   highlight = {
-    enable = true,
-  },
+    additional_vim_regex_highlighting = true
+  }
 }
 require("lualine").setup {}
 require("nvim-autopairs").setup{}
